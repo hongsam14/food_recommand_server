343ad39980a973944a06722173fe6de50133cd61
hongsam14 <69339846+hongsam14@users.noreply.github.com>
2021-11-17 15:54:58 +0900

    add first cf recommand

--- inner_api/food_recommand/views.py
+++ inner_api/food_recommand/views.py
@@ -17,23 +17,7 @@ import json
 import random
 import pandas as pd
 import numpy as np
-
-def add_by_answer(food_table:list, a1:str, a2:str, a3:str):
-    a1_list = a1.split(',')
-    a2_list = a2.split(',')
-    a3_list = a3.split(',')
-    for i in food_table:
-        for j in a1_list:
-            if j in i[0][1]:
-                i[1] += 1
-        for k in a2_list:
-            if k in i[0][1]:
-                i[1] += 1
-        for l in a3_list:
-            if l in i[0][1]:
-                i[1] += 1
-    food_table.sort(key=lambda x: -x[1])
-    return food_table[:4]
+from sklearn.metrics.pairwise import cosine_similarity
 
 def pick_random(query, count:int):
     ret = []
@@ -44,42 +28,81 @@ def pick_random(query, count:int):
         pool.remove(pick)
     return ret
 
+def add_by_answer(food_table, a1:str, a2:str, a3:str):
+    a1_list = a1.split(',')
+    a2_list = a2.split(',')
+    a3_list = a3.split(',')
+    for i in food_table:
+        for j in a1_list:
+            if j in i[1]:
+                i[2] += 1
+        for k in a2_list:
+            if k in i[1]:
+                i[2] += 1
+        for l in a3_list:
+            if l in i[1]:
+                i[2] += 1
+    return food_table
+
+def cf_simple(user_id, food_id, food_matrix, user_similarity):
+    if food_id in food_matrix:
+        sim_scores = user_similarity[user_id - 1].copy()
+        food_rate = food_matrix[food_id - 1].copy()
+        none_eat_index = food_rate[food_rate == 0].index
+        food_rate = food_rate[food_rate != 0]
+        sim_scores = sim_scores.drop(none_eat_index)
+        mean_rating = np.dot(sim_scores, food_rate) / np.sum(sim_scores)
+    else:
+        mean_rating = None
+    return mean_rating
+
 def food_recommand(id, a1, a2, a3):
     try:
         member = Member.objects.filter(member_id=id)
     except Member.DoesNotExist:
         return None
     #init
-    _info = np.array(Food.objects.all().values_list('tag'))
-    av_card = np.zeros((Member.objects.all().count(), Food.objects.all().count()), int)
+    food_size = Food.objects.all().count()
+    _info = np.column_stack((Food.objects.all().values_list('food_id', 'tag'), np.zeros(food_size, int).astype(np.object)))
+    _card = np.zeros((Member.objects.all().count(), food_size))
     _liked = np.array(LikeFood.objects.all().values_list('member_id', 'food_id'), int)
     _disliked = np.array(DislikeFood.objects.all().values_list('member_id', 'food_id'), int)
     _selected = np.array(SelectedFood.objects.all().values_list('member_id', 'food_id'), int)
-    print(_selected)
     #좋아하는 음식 +
     for i in _liked:
-        av_card[i[0] - 1, i[1] - 1] += 1
+        _card[i[0] - 1, i[1] - 1] += 1
     #싫어하는 음식 -
     for i in _disliked:
-        av_card[i[0] - 1, i[1] - 1] -= 1
+        _card[i[0] - 1, i[1] - 1] -= 1
     #먹었던 음식 +
     for i in _selected:
-        av_card[i[0] - 1, i[1] - 1] += 1
-    print(av_card)
-    #임시
-    food_table = [[i, 0] for i in Food.objects.all().values_list('food_id', 'tag')]
+        _card[i[0] - 1, i[1] - 1] += 1
+    user_similarity = cosine_similarity(_card, _card)
+    user_similarity = pd.DataFrame(user_similarity, index=np.arange(_card.shape[0]), columns=np.arange(_card.shape[0]))
+    food_matrix = pd.DataFrame(_card, index=np.arange(_card.shape[0]), columns=np.arange(_card.shape[1]))
+    print(food_matrix)
     ret = []
-    for i in add_by_answer(food_table, a1, a2, a3):
-        ret += Food.objects.filter(food_id=i[0][0])
+    _answer = add_by_answer(_info, a1, a2, a3)
+    print(_answer)
+    for i in _answer[_answer[:, 2] > 0]:
+        add = cf_simple(user_id=int(id), food_id=int(i[0]), food_matrix=food_matrix, user_similarity=user_similarity)
+        if add != None:
+            if np.isnan(add) == False:
+                i[2] += add
+    shuffle = np.random.permutation(_answer)
+    sorted = shuffle[shuffle[:, 2].argsort()][::-1]
+    print(sorted)
+    for i in sorted[:4]:
+        ret += Food.objects.filter(food_id=int(i[0]))
     return {"pickFood":ret}
-    
+
 @csrf_exempt
 @api_view(['GET'])
 def pickFood(request):
     if request.method == 'GET':
-        email = request.GET.get('id', None)
+        id = request.GET.get('id', None)
         a1 = request.GET.get('answer1', None)
         a2 = request.GET.get('answer2', None)
         a3 = request.GET.get('answer3', None)
-        serializer = ResultSerializer(food_recommand(email, a1, a2, a3))
-        return JsonResponse(serializer.data, json_dumps_params={'ensure_ascii': False})
+        serializer = ResultSerializer(food_recommand(id, a1, a2, a3))
+        return JsonResponse(serializer.data)
